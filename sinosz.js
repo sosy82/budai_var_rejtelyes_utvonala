102b8a768042d3bee3b068c1d0b981a7a06794de
sosy82 <szep.katalin.sosy@gmail.com>
2023-06-04 23:00:18 +0200

    Készen vagyunk

---
+++ sinosz.js
@@ -0,0 +1,22 @@
+function Szamol()
+{
+    var felnott_ar=10000;
+    var diak_ar=5000;
+    var nyugdijas_ar=5000;
+    var iskolas_ar=2000;
+    var ovodas_ar=200;
+
+    //hogy van az adatbekérés?
+    //mi a megoldása?
+
+    var felnott_db=document.getElementById("felnott").value;
+    var diak_db=document.getElementById("diak").value;
+    var nyugdijas_db=document.getElementById("nyugdijas").value;
+    var iskolas_db=document.getElementById("iskolas").value;
+    var ovodas_db=document.getElementById("ovodas").value;
+
+
+
+
+    
+}
